ec2f2e5c5cb93f1c96bfb7b497b19ea1dd4db272
vaibhav77471 <100358265+vaibhav77471@users.noreply.github.com>
2022-08-29 10:49:31 +0530

    Add files via upload

---
+++ patterns.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+/*
+//program of half tringle
+int main(){
+    int n;
+    cout<<"enter the no. of rows : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}*/
+/*
+int main(){
+    int n;
+    cout<<"enter no. of rows : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int s=1;s<=n-i;s++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+/*
+//program of pyramid
+int main(){
+    int n;
+    cout<<"enter the no. of rows : ";
+    cin>>n;
+    for(int i=1; i<=n;i++){
+        for(int s=1;s<=n-i;s++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        for(int k=1;k<=i; k++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}*/
+
+int main(){
+    int n;
+    cout<<"enter the no. of rows : ";
+    cin>>n;
+    int count=1;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<count;
+            count++;
+        }
+    cout<<endl;
+    }
+    return 0;
+}
+/*
+int main(){
+    int n;
+    cout<<"enter no. of rows to print squar : ";
+    cin>>n;
+    int count=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
